785f12795fc9993df69d48931a2fc1d88f8e5b21
Poojitha <poojitha.mamidi@impressico.com>
2025-05-26 16:55:36 +0530

    Added workflow

---
+++ workflows/step2_graph.py
@@ -0,0 +1,18 @@
+from langgraph.graph import StateGraph, END
+from nodes.Spec_file_generation import fetch_readme, extract_login_info, generate_spec
+from nodes.Spec_file_generation import AppState
+
+def create_spec_file_generation_graph():
+    graph = StateGraph(AppState)
+    graph.add_node("FetchReadme", fetch_readme)
+    graph.add_node("ExtractLoginInfo", extract_login_info)
+    graph.add_node("GenerateSpec", generate_spec)
+
+    graph.set_entry_point("FetchReadme")
+    graph.add_edge("FetchReadme", "ExtractLoginInfo")
+    graph.add_edge("ExtractLoginInfo", "GenerateSpec")
+    graph.set_finish_point("GenerateSpec")
+
+    app = graph.compile()
+
+   
