dde45928d89601a0d8f1f1389aee6ef139e1c394
Poojitha <poojitha.mamidi@impressico.com>
2025-06-01 17:37:19 +0530

    added state schema

---
+++ nodes/state_schemas.py
@@ -0,0 +1,14 @@
+from typing import TypedDict, Optional
+
+class AppState(TypedDict):
+    github_url: str
+    readme: str
+    login_context: str
+    spec_md: str
+    login_spec: str
+    login_url: str
+    page_html: str
+    selenium_code_path: str
+    image_path: Optional[str]
+    driver_path: Optional[str]
+    retry_count: Optional[int]
