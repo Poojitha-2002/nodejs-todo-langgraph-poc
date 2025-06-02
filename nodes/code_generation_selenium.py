399158efb037b554e709f0d0549591a31a0ff2fe
nishal-sr <nishal.sr@impressico.com>
2025-06-02 10:38:18 +0530

    Added print statement

--- nodes/code_generation_selenium.py
+++ nodes/code_generation_selenium.py
@@ -86,6 +86,8 @@ def generate_selenium_code(state: AppState) -> dict:
     with open(output_path, "w", encoding="utf-8") as f:
         f.write(generated_code)
 
+    print("Selenium code generated successfully!")
+
     return {
         "selenium_code_path": output_path,
         "retry_count": retry_count + 1
